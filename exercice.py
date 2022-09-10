d23047be0bc71ba0cc5411606619054e66a7f1da
mberg <javier.berger@polymtl.com>
2022-09-10 17:04:41 -0400

    final commit

--- exercice.py
+++ exercice.py
@@ -17,7 +17,7 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return math.radians(angle_degs), angle_mins/60*(2*math.pi), angle_secs/60*(2*math.pi)
+    return (math.radians(angle_degs) + angle_mins/60*(2*math.pi) + angle_secs/60*(2*math.pi))
 
 
 def to_degrees(angle_rads: float) -> tuple:
